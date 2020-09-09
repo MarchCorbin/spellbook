cea2f475600e15a25b73c669613071bc69645d56
Corbin March <marchcorbin@gmail.com>
2020-09-09 16:56:26 -0600

    Working buttons on the home page

---
+++ src/components/spellindex/SpellIndex.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>This is the Spell Index Page!</h1>
+</template>
+
+
+
+<script>
+// import Home from '../../views/Home.vue'
+export default {
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div>
+<h1 class="title">Hogwarts Spellbook ed.XVLVIIIXV</h1>
+
+<div class='button-holder'>
+<router-link to='/createspell'><button>Create a Spell!</button></router-link>
+<router-link to='/spellindex'><button>View Spells!</button></router-link>
+</div>
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import CreateSpell from '../components/createspell/CreateSpell.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
+
+<style scoped>
+.button-holder {
+  display: flex;
+  justify-content: space-around;
+}
+
+.title {
+    margin-top: 4em;
+    margin-bottom: 5em;
+}
+
+</style>
