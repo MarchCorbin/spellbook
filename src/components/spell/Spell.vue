6f6787c446a6e9ce135f93871bcb36b9f7971d9f
Sean Leas <phoenix_24leas.com>
2020-09-10 10:51:59 -0600

    Set up for cards

---
+++ src/components/spell/Spell.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{spell.spell}</h1>
+</template>
+
+<script>
+import SpellIndex from '../spellindex/SpellIndex.vue'
+
+export default {
+  name: 'Spell',
+  props: [spell] 
+
+}
+</script>
+
+<style scoped>
+
+</style>
