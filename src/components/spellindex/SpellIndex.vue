7e14137e099e73404eb4c8db015de47876e4d828
Sean Leas <phoenix_24leas.com>
2020-09-10 15:46:55 -0600

    Styling

--- src/components/spellindex/SpellIndex.vue
+++ src/components/spellindex/SpellIndex.vue
@@ -1,17 +1,22 @@
 <template>
-  <ul>
-    <div v-bind:key='spell._id' v-for='spell in allSpells'>
-      <h1>{{spell.spell}}</h1>
-      <h3 v-bind:spell='spell'>{{spell.type}}</h3>
-      <h3 v-bind:spell='spell'>{{spell.effect}}</h3>
-    </div>
-  </ul>
+  <div>
+    <Header />
+    <ul>
+
+      <div class='spell-card' v-bind:key='spell._id' v-for='spell in allSpells'>
+        <h1>{{spell.spell}}</h1>
+        <h3 v-bind:spell='spell'>{{spell.type}}</h3>
+        <h3 v-bind:spell='spell'>{{spell.effect}}</h3>
+      </div>
+    </ul>
+  </div>
 </template>
 
 
 
 <script>
 // import Home from '../../views/Home.vue'
+import Header from '../../'
 export default {
   props: {
     // allSpells : String 
@@ -38,3 +43,25 @@ export default {
 
 
 </script>
+
+<style scoped>
+  ul {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    
+  }
+
+  .spell-card {
+    border-color: black;
+    border: 5px solid;
+    margin: 1em;    
+  }
+
+@media screen and (max-width: 750px) {
+  ul {
+    grid-template-columns: 1fr 1fr
+  }
+}
+
+
+</style>
