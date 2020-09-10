7c56224d579111ec437d77525e902efe1d91fa7c
Sean Leas <phoenix_24leas.com>
2020-09-10 10:51:06 -0600

    Create data, method and a created method with routerlinks

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,46 @@
 <template>
-<div>
-<h1 class="title">Hogwarts Spellbook ed.XVLVIIIXV</h1>
+  <div>
+    <h1 class="title">Hogwarts Spellbook ed.XVLVIIIXV</h1>
 
-<div class='button-holder'>
-<router-link to='/createspell'><button>Create a Spell!</button></router-link>
-<router-link to='/spellindex'><button>View Spells!</button></router-link>
-</div>
-</div>
+    <div class='button-holder'>
+      <router-link to='/createspell'>
+        <button>Create a Spell!</button>
+      </router-link>
+      <router-link to='/spellindex'>
+        <button>View Spells!</button>
+      </router-link>
+      <SpellIndex v-bind:allSpells=this.allSpells />
+    </div>
+  </div>
 </template>
 
 <script>
+
 // @ is an alias to /src
 // import CreateSpell from '../components/createspell/CreateSpell.vue'
+import SpellIndex from '../components/spellindex/SpellIndex'
 
 export default {
+
   name: 'Home',
   components: {
-    // HelloWorld
-  }
+   SpellIndex
+  },
+  data() {
+    return {
+      allSpells: []
+    }
+  },
+  created() {
+    this.getAllSpells()
+  },
+  methods: {
+    getAllSpells () {
+      fetch('https://www.potterapi.com/v1/spells?key=$2a$10$m4giiYReoHdY5vLc5OsvxOPchfJHMDP0afjPdh/CN03cv/vc0SAl2')
+      .then(response => response.json())
+      .then(data => this.allSpells = data)
+    }
+  }   
 }
 </script>
 
