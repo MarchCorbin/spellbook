46a545cd51a8527261a01fc3e8e14385680dbf3b
Sean Leas <phoenix_24leas.com>
2020-09-10 11:28:12 -0600

    Transfer methods from Home to SpellIndex

--- src/components/spellindex/SpellIndex.vue
+++ src/components/spellindex/SpellIndex.vue
@@ -1,8 +1,10 @@
 <template>
   <ul>
-    <li v-bind:key='spell._id' v-for='spell in allSpells'>
-     <h3 v-bind:spell='spell'>{{spell._id}}</h3>
-    </li>
+    <div v-bind:key='spell._id' v-for='spell in allSpells'>
+      <h1>{{spell.spell}}</h1>
+      <h3 v-bind:spell='spell'>{{spell.type}}</h3>
+      <h3 v-bind:spell='spell'>{{spell.effect}}</h3>
+    </div>
   </ul>
 </template>
 
@@ -12,25 +14,24 @@
 // import Home from '../../views/Home.vue'
 export default {
   props: {
-    allSpells : String 
+    // allSpells : String 
   },
   name: 'SpellIndex',
-  // data() {
-  //   return {
-  //    allspells: []
-  //   }
-  // }
-  // beforeMount() {
-  //   this.getAllSpells()
-  // },
-
-  // methods: {
-  //   getAllSpells () {
-  //     fetch('https://www.potterapi.com/v1/spells?key=$2a$10$m4giiYReoHdY5vLc5OsvxOPchfJHMDP0afjPdh/CN03cv/vc0SAl2')
-  //     .then(response => response.json())
-  //     .then(data => this.allspells = data)
-  //   }
-  // }   
+  data() {
+    return {
+      allSpells: []
+    }
+  },
+  created() {
+    this.getAllSpells()
+  },
+  methods: {
+    getAllSpells () {
+      fetch('https://www.potterapi.com/v1/spells?key=$2a$10$m4giiYReoHdY5vLc5OsvxOPchfJHMDP0afjPdh/CN03cv/vc0SAl2')
+      .then(response => response.json())
+      .then(data => this.allSpells = data)
+    }
+  }   
 }
 
 
