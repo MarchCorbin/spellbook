19c9b1d158b512ac163fce5ccd5d6dff609ae5e2
Sean Leas <phoenix_24leas.com>
2020-09-10 15:46:08 -0600

    Set up Header component

---
+++ src/views/header/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav>
+    <h1>The Standard Book of Spells by Miranda Goshawk: Year 1</h1>
+    <!-- <img src='' class='header-logo' alt='Hogwarts spell book' /> -->
+    <div class='button-holder'>
+      <router-link to='/createspell'>
+        <button>Create a New Spell</button>
+      </router-link>
+      <router-link to='/' >
+        <button>Search</button>
+      </router-link>
+    </div>
+  </nav>
+</template>
+
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+  nav {
+    border: black solid 1px;
+  }
+</style>
