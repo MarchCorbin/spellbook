f6a56fb0c1e7b275ed3abdf50e67f3952afe0145
Corbin March <marchcorbin@gmail.com>
2020-09-11 19:01:17 -0600

    2 passing tests for the header component

--- tests/unit/Header.spec.js
+++ tests/unit/Header.spec.js
@@ -1,22 +1,28 @@
 import Header from '@/components/header/Header.vue'
-import { shallowMount, createLocalVue, mount } from '@vue/test-utils'
+import { shallowMount, createLocalVue, mount, waitFor } from '@vue/test-utils'
 import VueRouter from 'vue-router'
 
 describe('Header.vue', () => {
-  let wrapper;
+  let wrapper, picture
+  
   beforeEach(() => {
-    const localVue = createLocalVue()
+   
+    // const localVue = createLocalVue()
     // const router = new VueRouter()
-    localVue.use(VueRouter)
-    wrapper = mount(Header, {
-localVue,
-
-    })
+    // localVue.use(VueRouter)
+    wrapper = mount(Header)
+    
   })
-  it('render on load', () => {
-{
-  console.log('IAMWORKIGN')
-}
+  it('renders on load', () => {
 expect(wrapper.text()).toContain('The Standard Book of Spells by Miranda Goshawk: Year 1')
   })
+  it('should have two buttons', () => {
+expect(wrapper.text()).toContain('Create a New Spell')
+expect(wrapper.text()).toContain('Spell Index')
+  })
+  // it('should have an image', async() => {
+  //   picture = wrapper.find('header-logo')
+  //   console.log(picture, 'IAMPICTURE') 
+  //  expect(picture.is('.header-logo')).toBe(true)
+  // })
 })
