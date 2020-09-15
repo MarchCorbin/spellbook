5bbb708353430a72b180bf1bcc0e54b6ac9ab22f
Corbin March <marchcorbin@gmail.com>
2020-09-14 20:27:50 -0600

    Now have 16 passing test suite

--- tests/unit/SpellIndex.spec.js
+++ tests/unit/SpellIndex.spec.js
@@ -43,8 +43,29 @@ describe('SpellIndex', () => {
   })
   it('should render all of the spells', () => {
     const spellCards = wrapper.findAll('.spell-card')
-    console.log(wrapper)
-    console.log(spellCards, 'IAMSPELLCARDS')
     expect(spellCards.length).toBe(2)
+  })
+  it('should have 8 filter buttons', () => {
+    expect(wrapper.text()).toContain("Curse");
+    expect(wrapper.text()).toContain("Spell");
+    expect(wrapper.text()).toContain("Charm");
+    expect(wrapper.text()).toContain("Hex");
+    expect(wrapper.text()).toContain("Enchantment");
+    expect(wrapper.text()).toContain("Jinx");
+    expect(wrapper.text()).toContain("All Spells");
+    expect(wrapper.text()).toContain("Personal");
+  })
+  it('should render the title', () => {
+    expect(wrapper.text()).toContain(
+      "The Standard Book of Spells by Miranda Goshawk: Year 1"
+    );
+  })
+  it('should render the navigation buttons', () => {
+    expect(wrapper.text()).toContain("Create a New Spell");
+    expect(wrapper.text()).toContain("Spell Index");
+  })
+  it('should have 10 buttons on the page!', () => {
+    const buttons = wrapper.findAll('button')
+    expect(buttons.length).toBe(11)
   })
 })
