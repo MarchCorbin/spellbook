65518665355cef2ff0abc5951335555d040f27c6
Sean Leas <phoenix_24leas.com>
2020-09-14 21:41:09 -0600

    Style header icons, add media query

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,8 +1,9 @@
 <template>
   <nav>
-    <div class='header-title'>
-      <router-link  to='/'>
-          <img src='/assets/illuminati.png' class='header-logo' alt='Hogwarts spell book' /><button class='header-title'>The Standard Book of Spells by Miranda Goshawk: Year 1</button>
+    <div >
+      <router-link class='header-title' to='/'>
+          <img src='/assets/illuminati.png' class='header-logo' alt='Hogwarts spell book' />
+          <button class='header-title'>The Standard Book of Spells by Miranda Goshawk: Year 1</button>
           <img src='/assets/illuminati.png' class='header-logo' alt='Hogwarts spell book' />
       </router-link>  
       
@@ -42,11 +43,21 @@ export default {
   }
   .header-title {
     text-decoration: none;
-    border: none
+    border: none;
+    display: flex;
   }
 
   .router-link-active {
     box-shadow: inset #000000 0 0 60px;
     text-shadow: 0px -2px 4px #fff;
   }
+
+  @media screen and (max-width: 670px) {
+    .header-title {
+          flex-direction: column;
+    align-items: center;
+    margin: -36px;
+    }
+  }
+
 </style>
